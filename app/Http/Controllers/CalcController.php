740593388ee367124b814b82377dc5538ef0d008
dmiyakoshi <d1m354dai@gmail.com>
2021-09-13 18:08:27 +0900

    viewの中でしていた処理をコントーローラで行うよう 変更を行いました

--- app/Http/Controllers/CalcController.php
+++ app/Http/Controllers/CalcController.php
@@ -8,6 +8,25 @@ class CalcController extends Controller
 {
     public function result($number1, $how, $number2)
     {
-        return view('calc/result', ['number1' => $number1, 'how' => $how, 'number2' => $number2]);
+        $result = 0;
+
+        if ($how == 'addition')
+        {
+            $result = $number1 + $number2;
+        }
+        elseif ($how == 'subtraction')
+        {
+            $result = $number1 - $number2;
+        }
+        elseif ($how == 'multiplication')
+        {
+            $result = $number1 * $number2;
+        }
+            elseif ($how == 'division')
+        {
+            $result = $number1 / $number2;
+        }
+
+        return view('calc/result', ['result' => $result]);
     }
 }
--- resources/views/calc/result.blade.php
+++ resources/views/calc/result.blade.php
@@ -6,23 +6,6 @@
     </head>
     <body>
         <h1>計算アプリ</h1>
-        <?php
-            if ($how == 'addition')
-            {
-                echo $number1 + $number2;
-            }
-            elseif ($how == 'subtraction')
-            {
-                echo $number1 - $number2;
-            }
-            elseif ($how == 'multiplication')
-            {
-                echo $number1 * $number2;
-            }
-            elseif ($how == 'division')
-            {
-                echo $number1 / $number2;
-            }
-        ?>
+        <p>{{ $result }}<p>
     </body>
 </html>
